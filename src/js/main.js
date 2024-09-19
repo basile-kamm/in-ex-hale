1c357c7fb0804217aa56fea49c0b7cd514e79db1
basile-kamm <another-black-kitty@proton.me>
2024-09-19 17:10:54 +0200

    cicle annim

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+import { gsap } from "gsap";
+
+gsap.from("span", {
+  opacity: 0,
+  stagger: 0.4,
+});
+
+const circle = document.querySelector(".circle");
+
+gsap.to(circle, {
+  scale: 1.5,
+  repeat: -1,
+  yoyo: true,
+  duration: 3,
+  ease: "power1.inOut",
+});
